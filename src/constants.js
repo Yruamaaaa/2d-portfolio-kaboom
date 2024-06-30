7da0e275e824870047d74c0e9ae2730446569098
bodina <amaury.bodin@edu.esiee.fr>
2024-06-30 13:57:44 +0900

    game

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Python and a bit of R.
+    Here is my <a href="https://github.com/Yruamaaaa">Github</a>!`,
+  "cs-degree": `This is my Engeneering Degree from ESIEE PARIS, where I specialized in Data Science and Artificial Intelligence`,
+  "sofa-table": `That's my sofa. I like to relax here after hitting the gym. `,
+  tv: `That's my TV. I've been using youtube to sharpen my skills in Data visualization, Phyton Programming and Entrepreneurship `,
+  bed: `This where I sleep. Sleep is one of the most important asset for a person. It's only if you slept well that you can be productive`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Yruamaaaa/Resume-CV/blob/main/englishcv.pdf" target="_blank">Check it out?</a>
+  Contact me at amaurybo19@gmail.com if you have any questions !`,
+  projects: `I studied abroad for 4 months in Brno, Czech Republic at VUT in electrical engeneering. And also 
+  did a research project in Tokyo, Japan at Kogakuin University. `,
+  library: `There are a lot of books in the bookshelf. A lot of them are about improving yourself. Like for exemple Essentialism by Greg Mckeown`,
+  exit: `If you want to exit Amaury's portfolio, just close the tab.`,
+};
